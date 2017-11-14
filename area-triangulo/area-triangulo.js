546ee73e9bb01f80dfa1fe28c1038d5e2183e063
Yomar Miranda <mirandayomar@gmail.com>
2017-11-13 22:42:23 -0500

    Como podemos ver esta es otra manera de crear una función utilizando Arrow Function y de igual manera sigue funcionando.

--- area-triangulo/area-triangulo.js
+++ area-triangulo/area-triangulo.js
@@ -1,8 +1,14 @@
 let base = 6
 let altura = 9
 
-let areaTriangulo = function(base, altura){
+/*let areaTriangulo = function(base, altura){
+	return (base * altura) / 2
+}*/
+
+let areaTriangulo = (base, altura) => {
 	return (base * altura) / 2
 }
+
+
 
 console.log(`El área del triángulo de base ${base} y altura ${altura} es: ${areaTriangulo(base, altura)}`)
