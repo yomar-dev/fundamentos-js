26ac037dc2157f203693deeafc84c2f1fdeca3cf
Yomar Miranda <mirandayomar@gmail.com>
2017-11-13 22:35:16 -0500

    En este ejercicio vemos como calcular el área de un triángulo, para eso hemos escrito una función que nos realice ese cálculo, algo muy importante a tener en cuenta es para no andar concatenando variables hemos aprovechado una nueva caracteristica de ES6 que nos permite utilizar comillas invertidas y dentro de ellas podemos llamar funciones, variables, etc, pero para que eso funcione hay que encerrar dichas expresiones dentro del un signo pesos seguido de una llave que abre y otra que cierra.

---
+++ area-triangulo/area-triangulo.js
@@ -0,0 +1,8 @@
+let base = 6
+let altura = 9
+
+function areaTriangulo(base, altura){
+	return (base * altura) / 2
+}
+
+console.log(`El área del triángulo de base ${base} y altura ${altura} es: ${areaTriangulo(base, altura)}`)
