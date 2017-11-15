a06ef85324da06700ca9a223be0d000e7ea72e2e
Yomar Miranda <mirandayomar@gmail.com>
2017-11-14 22:27:41 -0500

    En esta ocasión creamos otro usuario, también agregamos una nueva condición la cual consiste en que un ñino viene acompañado de un adulto puede pasar a ver la pelicula aunque no tenga la edad minima exigida.

--- ejemplo-condicionales/condicionales.js
+++ ejemplo-condicionales/condicionales.js
@@ -7,15 +7,21 @@ const edad01 = 22
 const nombre02 = 'Javier'
 const edad02 = 10
 
+const nombre03 = 'Leonel'
+const edad03 = 11
 
-function puedeVerPelicula(nombre, edad){
+
+function puedeVerPelicula(nombre, edad, estaConAdulto){
 	if(edad >= edadMinima){
-		alert(`${nombre}, puedes pasar a ver la película: ${pelicula}`)
+		alert(`${nombre}, puedes pasar a ver la película: ${pelicula}.`)
+	}else if(estaConAdulto){
+		alert(`${nombre}, puedes pasar a ver la película: ${pelicula}, aunque tu edad es ${edad} vienes acompañado de un adulto.`)		
 	}else{
-		alert(`${nombre}, no puedes pasar a ver la película ${pelicula}. \nLa edad minima para ver la película es ${edadMinima} años y tu tienes ${edad}`)
+		alert(`${nombre}, no puedes pasar a ver la película ${pelicula}. \nLa edad minima para ver la película es ${edadMinima} años y tu tienes ${edad}.`)
 	}
 }
 
 
 puedeVerPelicula(nombre01, edad01)
 puedeVerPelicula(nombre02, edad02)
+puedeVerPelicula(nombre03, edad03, true)
