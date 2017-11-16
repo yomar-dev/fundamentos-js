6627b564a0357e60c9055805aee8a36e65e88254
Yomar Miranda <mirandayomar@gmail.com>
2017-11-15 21:44:13 -0500

    Creamos la tercera condición, la cual consiste en evaluar la palabra traducida y si tiene más de 10 letras de divide a la mitad y se separa por un guión medio (-).

--- platzom/platzom.js
+++ platzom/platzom.js
@@ -12,19 +12,35 @@ function platzom(str){
 		translation += 'pe'
 	}
 
+
+	/*
+		Si la palabra traducida tiene 10 o más letras, se debe
+		dividir a la mitad y unir con un guión medio (-)
+	*/
+	const length = translation.length
+	if(length >= 10){
+		const firstHalf = translation.slice(0, Math.round(length / 2))
+		const secondHalf = translation.slice(Math.round(length / 2))
+		translation = `${firstHalf}-${secondHalf}`
+	}
+
 	return translation
 }
 
 console.log(platzom("Programar")) // Program
 console.log(platzom("Zorro")) // Zorrope
 console.log(platzom("Zarpar")) // Zarppe
+console.log(platzom("abecedario")) // abece-dario
 
 /*
  	toLowerCase()	=>	Sirve para convertir una cadena de caracteres en minúsculas.
  	endsWith()		=>	Devuelve TRUE si una cadena de caracteres termina con los caracteres
 						pasados por parametro, de lo contrario devuelve FALSE.
+	startsWith()	=>	Devuelve TRUE si una cadena de caracteres empieza con los caracteres
+						pasados por parametro, de lo contrario devuelve FALSE.
  	slice()			=>	Sirve para extraer parte de una cadena de caracteres indicando
  						el principio y final de la misma.
  						Ej: str.slice(0, -2)
  						Esto traera desde la posición 0 hasta 2 caracteres antes del final.
+ 	Math.round()	=>	Retorna el valor de un número redondeado al entero más cercano.
 */
