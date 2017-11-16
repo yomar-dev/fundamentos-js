3d8fedefea18b70ad6f31bd5324e9bda972eaf31
Yomar Miranda <mirandayomar@gmail.com>
2017-11-15 22:20:38 -0500

    Creamos la última condición, la cual consiste en validar si una palabra original es un palíndromo, ninguna regla anterior se tiene en cuenta y se devuelve la misma palabra intercalando minúsculas y mayúsculas, para realizar dicha validación se crearon dos funciones, una para invertir dicha palabra y más tarde compararla con la original, y otra para intercalar entre mayúsculas y minúsculas.

--- platzom/platzom.js
+++ platzom/platzom.js
@@ -1,13 +1,16 @@
 function platzom(str){
 	let translation = str
 
-	// Si la palabra termina en "ar", se le quitan esos dos caracteres.
+	/*
+		Si la palabra termina en "ar", se le quitan esos dos caracteres.
+	*/
 	if(str.toLowerCase().endsWith('ar')){
 		translation = str.slice(0, -2)
 	}
 
-
-	// Si la palabra termina con Z, se le añade "pe" al final.
+	/*
+		Si la palabra termina con Z, se le añade "pe" al final.	
+	*/
 	if(str.toLowerCase().startsWith('z')){
 		translation += 'pe'
 	}
@@ -24,13 +27,47 @@ function platzom(str){
 		translation = `${firstHalf}-${secondHalf}`
 	}
 
+
+	/*
+		Si la palabra original es un palíndromo, ninguna regla anterior
+		se tiene en cuenta y se devuelve la misma palabra intercalando
+		mayúsculas y minúsculas.  
+	*/
+	if(str == reverse(str)){
+		return minMay(str)
+	}
+
 	return translation
 }
+
+/*
+	Devuelve una cadena de caracteres invertida.
+*/
+const reverse = (str) => str.split('').reverse().join('')
+
+/*
+	Devuelve una cadena de caracteres intercalando Mayúsculas y Minúsculas.
+*/
+function minMay(str){
+	const length = str.length
+	let translation = ''
+	let capitalize = true
+	for (let i = 0; i < length; i++) {
+		const char = str.charAt(i)
+		translation += capitalize ? char.toUpperCase() : char.toLowerCase()
+		capitalize = !capitalize
+	}
+
+	return translation
+}
+
+
 
 console.log(platzom("Programar")) // Program
 console.log(platzom("Zorro")) // Zorrope
 console.log(platzom("Zarpar")) // Zarppe
 console.log(platzom("abecedario")) // abece-dario
+console.log(platzom("sometemos")) // SoMeTeMoS
 
 /*
  	toLowerCase()	=>	Sirve para convertir una cadena de caracteres en minúsculas.
@@ -43,4 +80,7 @@ console.log(platzom("abecedario")) // abece-dario
  						Ej: str.slice(0, -2)
  						Esto traera desde la posición 0 hasta 2 caracteres antes del final.
  	Math.round()	=>	Retorna el valor de un número redondeado al entero más cercano.
+ 	split()			=>	Divide una cadena de caracteres por el valor que le indiquemos por parametro.
+ 	reverse()		=>	Invierte el orden de los elementos de un Array.
+ 	join()			=>	Une los elementos de un Array por el valor indicado por parametro.
 */
