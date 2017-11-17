957c9c1718e9360d304a79626c0520e64b71ba8a
Yomar Miranda <mirandayomar@gmail.com>
2017-11-16 23:33:17 -0500

    Creamos una variable en la cual vamos a acumular el total de kilómetros recorridos por una persona, para eso utilizamos un ciclo for, además mostramos a través de la consola cuantos kilómetros recorrio en determinado día.

--- kms-promedio-recorridos/kms.js
+++ kms-promedio-recorridos/kms.js
@@ -11,6 +11,14 @@ const dias = [
 ]
 
 
+let totalKms = 0
+
+for (let i = 0; i < dias.length; i++) {
+	const kms = correr()
+	totalKms += kms
+	console.log(`El día ${dias[i]}, ${nombre} corrió ${kms}Kms`)
+}
+
 /* 
 	Math.random()	=> Devuelve un número entre 0 y 1. 
 	Math.floor()	=> Redondea un número hacia abajo. Ej: 3.7 = 3 
