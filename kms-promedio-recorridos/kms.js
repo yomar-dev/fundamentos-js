1d716af6a31f3e494e221980b248e7c17b425332
Yomar Miranda <mirandayomar@gmail.com>
2017-11-16 23:54:37 -0500

    Hemos hecho dos cosas importantes, la primera es optimizar el código para hacerlo más eficiente, en este caso tomamos el tamaño del Array y lo almacenamos en una variable y luego lo utilizamos en el ciclo para no andar preguntando su tamaño en cada iteración, lo segundo que hemos hecho es indicar cuantas cifras decimales queremos mostrar del promedio de kilómetros recorridos.

--- kms-promedio-recorridos/kms.js
+++ kms-promedio-recorridos/kms.js
@@ -12,20 +12,21 @@ const dias = [
 
 
 let totalKms = 0
-
-for (let i = 0; i < dias.length; i++) {
+let length = dias.length
+for (let i = 0; i < length; i++) {
 	const kms = correr()
 	totalKms += kms
 	console.log(`El día ${dias[i]}, ${nombre} corrió ${kms}Kms`)
 }
 
-const promedioKms = totalKms / dias.length
-console.log(`En promedio ${nombre}, corrió ${promedioKms}Kms`)
+const promedioKms = totalKms / length
+console.log(`En promedio ${nombre}, corrió ${promedioKms.toFixed(2)}Kms`)
 
 /* 
 	Math.random()	=> Devuelve un número entre 0 y 1. 
 	Math.floor()	=> Redondea un número hacia abajo. Ej: 3.7 = 3 
 	Math.round()	=> Redondea al entero más cercano. 
+	toFixed()		=> Sirve para indicar cuantas cifras decimales queremos mostrar de un número.
 */
 function correr(){
 	const min = 5
