ce62cf46a667ba5eba4526e22902fd4113873139
Yomar Miranda <mirandayomar@gmail.com>
2017-11-16 23:25:55 -0500

    Creamos una función que nos va a devolver un valor aleatorio entre 5 y 15, que van a ser el número de Kms recorrido por una persona en determinado día.

--- kms-promedio-recorridos/kms.js
+++ kms-promedio-recorridos/kms.js
@@ -9,3 +9,14 @@ const dias = [
 	'Sábado',
 	'Domingo'
 ]
+
+
+/* 
+	Math.random()	=> Devuelve un número entre 0 y 1. 
+	Math.floor()	=> Redondea un número hacia abajo. Ej: 3.7 = 3 
+*/
+function correr(){
+	const min = 5
+	const max = 15
+	return Math.floor(Math.random() * (max - min) + min)
+}
