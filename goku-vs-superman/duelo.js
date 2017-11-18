30e3931c171de88f8a20fe8aec054825f6a45b7c
Yomar Miranda <mirandayomar@gmail.com>
2017-11-17 23:42:38 -0500

    Se creó una condición en la cual, quien tenga mayor golpe ataca y el valor del golpe sera descontado de la vida de su oponente, además en cada iteración se va mostrando por que Round va la pelea, también en cada Round se muestra el valor del golpe del atacante sera y la vida restante de su oponente.

--- goku-vs-superman/duelo.js
+++ goku-vs-superman/duelo.js
@@ -8,11 +8,24 @@ const ambosSiguenVivos = () => vidaGoku > 0 && vidaSuperman > 0
 
 const calcularGolpe = () => Math.round(Math.random() * MAX_POWER - MIN_POWER) + MIN_POWER
 
-let round = 1
+let round = 0
 
 while(ambosSiguenVivos()){
+	round++
 	console.log(`Round: ${round}`)
 
 	const golpeGoku = calcularGolpe()
 	const golpeSuperman = calcularGolpe()
+
+	if(golpeGoku > golpeSuperman){
+		//Ataca Goku
+		console.log(`Goku ataca con un golpe de: ${golpeGoku}`)
+		vidaSuperman -= golpeGoku
+		console.log(`Vida Superman: ${vidaSuperman}`)		
+	}else{
+		//Ataca Superman
+		console.log(`Superman ataca con un golpe de: ${golpeSuperman}`)
+		vidaGoku -= golpeSuperman
+		console.log(`Vida Goku: ${vidaGoku}`)		
+	}
 }
