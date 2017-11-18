e0f914eb0b48e06f234973e2b84595af41a19fef
Yomar Miranda <mirandayomar@gmail.com>
2017-11-17 23:20:34 -0500

    Hemos creado una función para validar si ambos luchadores siguen vivos, de ser asi devuelve TRUE en caso contrario FALSE, el valor retornado por dicha función lo vamos a utilizar para ejecutar un ciclo mientras el valor retornado sea TRUE (Ambos luchadores sigan cin vida).

--- goku-vs-superman/duelo.js
+++ goku-vs-superman/duelo.js
@@ -3,3 +3,13 @@ let vidaSuperman = 100
 
 const MIN_POWER = 5
 const MAX_POWER = 12
+
+function ambosSiguenVivos(){
+	return vidaGoku > 0 && vidaSuperman > 0
+}
+
+let round = 1
+
+while(ambosSiguenVivos()){
+
+}
