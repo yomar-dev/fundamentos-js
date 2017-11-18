ab1b2a2518f1abc76269fa165951864108ec069a
Yomar Miranda <mirandayomar@gmail.com>
2017-11-17 23:30:46 -0500

    Hemos creado una Arrow Function para calcular un golpe entre 5 y 12 de manera aleatoria, dentro del ciclo hemos creado dos constantes una para almacenar el valor del golpe de Goku y la otra el golpe de Superman, además imprimiremos el Round en el que va la pelea.

--- goku-vs-superman/duelo.js
+++ goku-vs-superman/duelo.js
@@ -6,8 +6,13 @@ const MAX_POWER = 12
 
 const ambosSiguenVivos = () => vidaGoku > 0 && vidaSuperman > 0
 
+const calcularGolpe = () => Math.round(Math.random() * MAX_POWER - MIN_POWER) + MIN_POWER
+
 let round = 1
 
 while(ambosSiguenVivos()){
+	console.log(`Round: ${round}`)
 
+	const golpeGoku = calcularGolpe()
+	const golpeSuperman = calcularGolpe()
 }
