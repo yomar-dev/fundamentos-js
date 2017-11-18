6b0c61e3f659eceaf3339d5a99b916602271b86d
Yomar Miranda <mirandayomar@gmail.com>
2017-11-17 23:22:58 -0500

    Hemos transformado la función 'ambosSiguenVivos()' en una Arrow Function para óptimizar el código.

--- goku-vs-superman/duelo.js
+++ goku-vs-superman/duelo.js
@@ -4,9 +4,7 @@ let vidaSuperman = 100
 const MIN_POWER = 5
 const MAX_POWER = 12
 
-function ambosSiguenVivos(){
-	return vidaGoku > 0 && vidaSuperman > 0
-}
+const ambosSiguenVivos = () => vidaGoku > 0 && vidaSuperman > 0
 
 let round = 1
 
