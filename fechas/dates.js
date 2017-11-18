0771bedc29e029e04d2f18208f450673eabe85c4
Yomar Miranda <mirandayomar@gmail.com>
2017-11-18 15:58:29 -0500

    He creado un objeto de tipo Date en el cual le paso mi fecha de nacimiento, en el caso del mes hay que restarle 1, debido a que los meses en Javascript empiezan desde 0, en mi caso mi mes de nacimiento es 12 (Diciembre) pero como hay que restar 1 he pasado el 11 al constructor.

---
+++ fechas/dates.js
@@ -0,0 +1,8 @@
+/*
+	Creamos un objeto de tipo fecha, en este caso le estamos asignando
+	mi fecha de nacimiento, pero hay que tener en cuenta que en el caso
+	de los menos hay que restarle 1, en este caso mi mes de nacimiento
+	es Diciembre (Mes 12), pero como hay que restarle 1, debo colocar
+	el mes 11. 
+*/
+const nacimiento = new Date(1992, 11, 30)
