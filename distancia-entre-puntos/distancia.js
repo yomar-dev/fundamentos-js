e9e0e49e534cfcf3dd7d3734eff6c0d588ce2236
Yomar Miranda <mirandayomar@gmail.com>
2017-11-19 00:24:48 -0500

    Hemos utilizado otra forma de crear objetos utilizando 'Object.create()', al cual le pasamos una función 'init' que va a funcionar como constructor y los metodos utilizados anteriormente para mover los puntos y cálcular la distancia entre dos puntos. Nota: Al final de cuenta esto también es un Prototipo.

--- distancia-entre-puntos/distancia.js
+++ distancia-entre-puntos/distancia.js
@@ -26,7 +26,7 @@
 	}
 }*/
 
-function Punto(x, y){
+/*function Punto(x, y){
 	this.x = x
 	this.y = y
 }
@@ -44,10 +44,34 @@ Punto.prototype.distancia = function distancia(p){
 	const y = this.y - p.y
 
 	return Math.sqrt((x * x) + (y * y)).toFixed(2)	
+}*/
+
+
+// Esta es otro forma de crear objetos.
+const Punto = {
+	// Funciona como un constructor.
+	init: function init(x, y){
+		this.x = x
+		this.y = y
+	},
+	moverEnX: function moverEnX(x) {
+		this.x += x
+	},
+	moverEnY: function moverEnY(y) {
+		this.y += y
+	},
+	distancia: function distancia(p){
+		const x = this.x - p.x
+		const y = this.y - p.y
+
+		return Math.sqrt((x * x) + (y * y)).toFixed(2)	
+	}
 }
 
-const p1 = new Punto(0, 4)
-const p2 = new Punto(3, 0)
+const p1 = Object.create(Punto)
+const p2 = Object.create(Punto)
+p1.init(0, 4)
+p2.init(3, 0)
 
 p1.moverEnX(7)
 p2.moverEnY(-2)
