3fa6a20cec210f17aeba28ffca435d0339001f28
Yomar Miranda <mirandayomar@gmail.com>
2017-11-19 19:51:28 -0500

    Esta vez creamos una nueva función la cual recibe por parametro una variable que coincide con el nombre de la variable declarada por fuera de la función, en este caso al momento de imprimir el nombre, mostrará el nombre pasado por parametro.

--- scope/scope-variables.js
+++ scope/scope-variables.js
@@ -1,7 +1,24 @@
 var nombre = "Yomar"
 
-function saludar(){
+/*
+	Si no pasamos parametros la variable 'nombre', mostrará por
+	consola, el nombre que se encuentra por fuera, en este caso 'Yomar'.
+*/
+/*function saludar(){
+	console.log(`Hola ${nombre}`)	
+}*/
+
+// saludar()
+
+/*
+	En esta función pasamos un parametro llamado nombre que coincide
+	con el nombre de la variable que se encuentra por fuera, en este
+	caso si tratamos de imprimir la variable nombre, no mostrara el valor
+	de la variable nombre que se encuentra por fuera sino el valor de la
+	variable que se le pase por parametro.
+*/
+function saludar(nombre){
 	console.log(`Hola ${nombre}`)
 }
 
-saludar()
+saludar("Erick")
