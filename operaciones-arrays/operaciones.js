732f041ba73034de331997920f4a23d6a6ba6f73
Yomar Miranda <mirandayomar@gmail.com>
2017-11-19 21:59:09 -0500

    En esta ocasión hemos visto como podemos sumar los valores de un Array utilizando la 'reduce' sin la necesidad de recorrer el Array utilizando un ciclo 'for'.

--- operaciones-arrays/operaciones.js
+++ operaciones-arrays/operaciones.js
@@ -8,12 +8,25 @@
 	a esto se le conoce como "Spread Operator". Si mostramos por consola el valor
 	de 'numeros' podemos observar que nos devuelve un Array
 */
-function sumar(...numeros){
+/*function sumar(...numeros){
 	let total = 0
 	for (let i = 0; i < numeros.length; i++) {
 		total += numeros[i]
 	}
 	return total;
+}*/
+
+/*
+	Función para sumar números utilizando una función propia de los Arrays
+	llamada 'reduce'
+	reduce()	=> Aplica una función a un acumulador y a cada valor de un
+	array (de izquierda a derecha) para reducirlo a un único valor.
+*/
+function sumar(...numeros){
+	return numeros.reduce(function(total, numero){
+		total += numero
+		return total
+	}, 0)
 }
 
 sumar(2, 5, 8, 9, 14)
