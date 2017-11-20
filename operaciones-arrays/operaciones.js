2ddc89fd9336c91e02b75a8f4a1e57b36ccb5a38
Yomar Miranda <mirandayomar@gmail.com>
2017-11-19 21:40:19 -0500

    Hemos creado una función que sumará N cantidad de números, y como no sabemos cuantos números se le van a pasar por parametro se le indica utilizando algo llamado 'Spread Operator' y al final será un Array que contiene todos los valores pasados por parametro.

--- operaciones-arrays/operaciones.js
+++ operaciones-arrays/operaciones.js
@@ -2,3 +2,14 @@
  *	Veremos operaciones avanzadas con Arrays, utilizando funciones de
  *	Javascript tales como: reduce, push, map y filter.
  */
+
+/*
+	Esta es la forma de pasar N parametros del mismo tipo sin espeficar la cantidad,
+	a esto se le conoce como "Spread Operator". Si mostramos por consola el valor
+	de 'numeros' podemos observar que nos devuelve un Array
+*/
+function sumar(...numeros){
+	console.log(numeros)
+}
+
+sumar(2, 5, 8, 9, 14)
