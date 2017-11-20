daeea5c86bf1eca7564f0ba48c1ae4b7712d0f62
Yomar Miranda <mirandayomar@gmail.com>
2017-11-19 19:05:31 -0500

    Hemos visto otra forma de crear objetos, esta es una forma mucho mejor y fácil de entender, utilizando esta sintaxis es obligatorio crear el constructor. Nota: Cabe aclarar que no son objetos como tal, en realidad son prototipos pero utilizamos una sintaxis mucho más amigable y fácil de entender.

--- distancia-entre-puntos/distancia.js
+++ distancia-entre-puntos/distancia.js
@@ -47,8 +47,8 @@ Punto.prototype.distancia = function distancia(p){
 }*/
 
 
-// Esta es otro forma de crear objetos.
-const Punto = {
+// Esta es otra forma de crear objetos.
+/*const Punto = {
 	// Funciona como un constructor.
 	init: function init(x, y){
 		this.x = x
@@ -66,12 +66,38 @@ const Punto = {
 
 		return Math.sqrt((x * x) + (y * y)).toFixed(2)	
 	}
+}*/
+
+
+/*
+	Utilizamos la palabra reservada 'class' pero en
+	realidad funciona como un Prototipo.
+*/
+class Punto{
+	// Este metodo siempre debe llamarse así.
+	constructor(x, y){
+		this.x = x
+		this.y = y
+	}
+
+	moverEnX(x) {
+		this.x += x
+	}
+
+	moverEnY(y) {
+		this.y += y
+	}
+
+	distancia(p){
+		const x = this.x - p.x
+		const y = this.y - p.y
+
+		return Math.sqrt((x * x) + (y * y)).toFixed(2)	
+	}
 }
 
-const p1 = Object.create(Punto)
-const p2 = Object.create(Punto)
-p1.init(0, 4)
-p2.init(3, 0)
+const p1 = new Punto(0, 4)
+const p2 = new Punto(3, 0)
 
 p1.moverEnX(7)
 p2.moverEnY(-2)
