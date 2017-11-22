598ba7f092909440ebb83c92ca29cafc76d67447
Yomar Miranda <mirandayomar@gmail.com>
2017-11-21 22:16:43 -0500

    Hemos creado una función para listar todos los amigos que se pasen por parametro al objeto persona, pero también hemos visto que tenemos que hacer para poder acceder a las propiedades globales del objeto, en este caso 'this.nombre'.

--- this-and-arrow-functions/object.js
+++ this-and-arrow-functions/object.js
@@ -3,7 +3,21 @@ class Persona{
 		this.nombre = nombre
 		this.amigos = amigos
 	}
+
+	listarAmigos(){
+		/**
+		 *	Para poder acceder a la propiedad 'nombre' dentro de la función que se
+		 *	ejecuta dentro del forEach, hay que crear una variable o una constante
+		 *	dentro de la función principal que recuerde el scope global, en este 
+		 *	caso 'listarAmigos()' que haga referencia al objeto 'this' de la clase 
+		 *	'Persona'.
+		 */
+		const _this = this
+		this.amigos.forEach(function(amigo) {
+			console.log(`Hola mi nombre es ${_this.nombre} y soy amigo de ${amigo}`)
+		})
+	}
 }
 
-
 const messi = new Persona("Messi", ["Ronaldinho", "Xavi", "Iniesta", "Henry"])
+messi.listarAmigos()
