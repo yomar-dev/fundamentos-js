2c8790d9cea174a5b0bcfb36a636d348662f6897
Yomar Miranda <mirandayomar@gmail.com>
2017-11-23 22:41:45 -0500

    Hemos definido la clase que vamos a utilizar y los metodos necesarios para el funcionamiento de la misma.

---
+++ bind/bind.js
@@ -0,0 +1,23 @@
+
+class Toggable{
+	/**
+	 * Inicializar el estado interno.
+	 */
+	constructor(element){
+
+	}
+
+	/**
+	 * Cambiar el estado interno y llamar a toggleText().
+	 */
+	onClick(){
+		
+	}
+
+	/**
+	 * Cambiar texto.
+	 */
+	toggleText(){
+
+	}
+}
