c0298f72f02a2b7afe7ed26703cc7c2aad4b9019
Yomar Miranda <mirandayomar@gmail.com>
2017-11-25 22:36:33 -0500

    Hemos hecho uso del metodo 'call()' con el cual le pasamos el contexto a la función (this) y luego los parametros requeridos por dicha función de forma individual separados por comas, cabe destacar que este ejemplo el objeto 'yomar' va a ser 'this'.

--- call-apply/core.js
+++ call-apply/core.js
@@ -1,7 +1,16 @@
+const yomar = {
+	nombre: 'Yomar',
+	apellido: 'Miranda'
+}
+
 function saludar(veces){
 	for (let i = 0; i < veces; i++) {
 		console.log(`Hola ${this.nombre} ${this.apellido}`)
 	}
 }
 
-saludar(5)
+/**
+ * call()	=>	llama a una función con un valor this asignado y 
+ * argumentos provistos de forma individual.
+ */
+saludar.call(yomar, 5)
