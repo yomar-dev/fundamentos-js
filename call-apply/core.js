93a037e7d9559f1c611dd1676724804686322403
Yomar Miranda <mirandayomar@gmail.com>
2017-11-25 22:46:24 -0500

    Ahora hemos usado el metodo 'apply()' para darle solución al mismo ejercicio, como podemos observar hace lo mismo que el metodo 'call()' la única diferencia es que con el metodo 'call()' recibe los parametros separados por comas y el metodo 'apply()' los parametros se los pasamos por medio de un Array, de echo para no pasar el Array con todos sus valores a la función podemos crear un Array con todos los valores y luego pasarlo al la función.

--- call-apply/core.js
+++ call-apply/core.js
@@ -10,7 +10,13 @@ function saludar(veces){
 }
 
 /**
- * call()	=>	llama a una función con un valor this asignado y 
+ * call()	=>	Llama a una función con un valor this asignado y 
  * argumentos provistos de forma individual.
  */
 saludar.call(yomar, 5)
+
+/**
+ * apply()	=>	Invoca una determinada función asignando explícitamente
+ * el objeto this y un array como parámetros para dicha función.
+ */
+saludar.apply(yomar, [5])
